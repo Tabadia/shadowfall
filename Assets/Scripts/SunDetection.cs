3b28efec4017c4b0e63bd0e77968410a9e3cd49a
Thalen <thalenabadia@gmail.com>
2024-01-06 14:31:08 -0800

    camera brightness effect when leaving or entering shadow
    took way too long :(

--- Assets/Scripts/SunDetection.cs
+++ Assets/Scripts/SunDetection.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SunDetection : MonoBehaviour
 {
 
     public Light directionalLight;
+    public GameObject darkenScreen;
+    public float screenAlpha;
     public bool inSun = false;
     public Vector3 lightAngle;
 
@@ -20,6 +23,7 @@ public class SunDetection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        screenAlpha = darkenScreen.GetComponent<Image>().color.a;
         int layerMask = 1 << 8;
 
         // This would cast rays only against colliders in layer 8.
@@ -31,12 +35,47 @@ public class SunDetection : MonoBehaviour
         if (Physics.Raycast(transform.position, lightAngle, out hit, Mathf.Infinity, layerMask))
         {
             Debug.DrawRay(transform.position, lightAngle * hit.distance, Color.yellow);
+            
+            if (inSun == true){
+                StartCoroutine(FadeIn());
+            }
             inSun = false;
+            
         }
         else
         {
             Debug.DrawRay(transform.position, lightAngle * 1000, Color.white);
+            
+            if (inSun == false){
+                StartCoroutine(FadeOut());
+            }
             inSun = true;
         }
+    }
+
+    IEnumerator FadeIn()
+    {
+        Image d = darkenScreen.GetComponent<Image>();
+        Color dc = d.color;
+        while (dc.a < 0.5f)
+        {
+            dc.a += 0.05f;
+            d.color = dc;
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return null;
+    }
+
+    IEnumerator FadeOut()
+    {
+        Image d = darkenScreen.GetComponent<Image>();
+        Color dc = d.color;
+        while (dc.a > 0.05f)
+        {
+            dc.a -= 0.05f;
+            d.color = dc;
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return null;
     }
 }
