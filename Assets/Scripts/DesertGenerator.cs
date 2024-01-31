12f07e2af722a0d8b53e525e693c56e05a08c2cc
ThalenAbadia <tabadia7574@student.pps.net>
2024-01-31 11:38:08 -0800

    Procedural desert generation progress!
    almost done ngl

--- Assets/Scripts/DesertGenerator.cs
+++ Assets/Scripts/DesertGenerator.cs
@@ -1,76 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Globalization;
-using System;
 using UnityEngine;
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))] //ensure object has mesh components
 
-public class DesertGenerator : MonoBehaviour
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class CustomTerrainGenerator : MonoBehaviour
 {
-	Mesh mesh;
-    List<float> heightList;
-	Vector3[] vertices;
-	int[] triangles;
+    Mesh mesh;
+    MeshFilter meshFilter;
+    Vector3[] vertices;
+    int[] triangles;
+    Vector2[] uv;
 
-    // set the size of the mesh base
-	public int xSize = 100;
-	public int zSize = 100;
+    // Set the size of the mesh base
+    public int totalSize = 300; // Total size of the terrain
+    public int flatSize = 100;  // Size of the flat center area
+    public int desertSize = 100; // Size of the surrounding desert area
+    public float strength = 0.3f;
 
-	public float strength = 0.3f;
+    const float HeightMultiplier = 2f;
 
-	void Start() {
-        // Setup the mesh
-		mesh = new Mesh();
-		GetComponent<MeshFilter>().mesh = mesh;
-        
-		CreateShape();
-		UpdateMesh();
-	}
+    void Start()
+    {
+        InitializeMesh();
+        CreateShape();
+        UpdateMesh();
+    }
 
+    void InitializeMesh()
+    {
+        mesh = new Mesh();
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+    }
 
-    void CreateShape() {
-		// print(xSize);
-		// print(zSize);
-		// print((xSize + 1) * (zSize + 1));
-		vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+    void CreateShape()
+    {
+        vertices = new Vector3[(totalSize + 1) * (totalSize + 1)];
+        uv = new Vector2[vertices.Length];
 
-		for (int i = 0, z = 0; z <= zSize; z++){
-			for (int x = 0; x <= xSize; x++){
-				float y = Mathf.PerlinNoise(x * strength, z * strength) * 2f;
-				vertices[i] = new Vector3(x, y, z);
-				i++;
-			}
-		}
-		triangles = new int[xSize * zSize * 6];
+        for (int i = 0, z = 0; z <= totalSize; z++)
+        {
+            for (int x = 0; x <= totalSize; x++)
+            {
+                float y = CalculateHeight(x, z);
+                vertices[i] = new Vector3(x, y, z);
+                uv[i] = new Vector2((float)x / totalSize, (float)z / totalSize);
+                i++;
+            }
+        }
 
-		int vert = 0;
-		int tris = 0;
+        CreateTriangles();
+    }
 
-		for (int z = 0; z < zSize; z++)
-		{
-			for (int x = 0; x < xSize; x++)
-			{
-				triangles[tris + 0] = vert + 0;
-				triangles[tris + 1] = vert + xSize + 1;
-				triangles[tris + 2] = vert + 1;
-				triangles[tris + 3] = vert + 1;
-				triangles[tris + 4] = vert + xSize + 1;
-				triangles[tris + 5] = vert + xSize + 2;
+    float CalculateHeight(int x, int z)
+    {
+        float flatCenterHeight = 0f;
+        float desertHeight = Mathf.PerlinNoise(x * strength, z * strength) * HeightMultiplier;
 
-				vert++;
-				tris += 6;
-			}
-			vert++;
-		}
-	}
+        // Check if the point is inside the flat center area
+        if (x >= (totalSize - flatSize) / 2 && x <= (totalSize + flatSize) / 2 &&
+            z >= (totalSize - flatSize) / 2 && z <= (totalSize + flatSize) / 2)
+        {
+            return flatCenterHeight; // Flat center area
+        }
+        else
+        {
+            return desertHeight; // Surrounding desert area
+        }
+    }
 
-	void UpdateMesh() {
+    void CreateTriangles()
+    {
+        triangles = new int[totalSize * totalSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < totalSize; z++)
+        {
+            for (int x = 0; x < totalSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + totalSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + totalSize + 1;
+                triangles[tris + 5] = vert + totalSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+    }
+
+    void UpdateMesh()
+    {
         mesh.Clear();
-
-		mesh.vertices = vertices;
-		mesh.triangles = triangles;
-
-		mesh.RecalculateNormals();
-	}
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uv;
+        mesh.RecalculateNormals();
+    }
 }
