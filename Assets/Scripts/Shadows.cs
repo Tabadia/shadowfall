ba88aa9da98ba718b0b25d3e4a017d76c039cb46
KryptKrawler <73673768+KryptKrawler@users.noreply.github.com>
2024-02-05 13:47:26 -0800

    Fixed Arbritrary Y Scaling (halfway done)

--- Assets/Scripts/Shadows.cs
+++ Assets/Scripts/Shadows.cs
@@ -21,6 +21,8 @@ public class Shadows : MonoBehaviour
         FixShadows(allObjects);
     }
 
+
+ 
     public void FixShadows(GameObject[] allObjects){
         for (int i = 0; i < allObjects.Length; i++){
             GameObject original = allObjects[i];
@@ -36,8 +38,22 @@ public class Shadows : MonoBehaviour
             }
 
             clone = Instantiate(original);
-            clone.transform.parent = original.transform;
-            
+            GameObject emptyParent = new GameObject("EmptyParent");
+
+            // Set the parent's position to match the originalObject
+            //emptyParent.transform.position = original.transform.position;
+
+            // Set the parent's rotation to identity (no rotation)
+            //emptyParent.transform.rotation = Quaternion.identity;
+            clone.transform.parent = emptyParent.transform;
+
+
+            // Reset the child's local position and rotation
+
+            emptyParent.transform.localPosition = Vector3.zero;
+            emptyParent.transform.localRotation = Quaternion.identity;
+            emptyParent.transform.localScale = new Vector3(1, 1, 1);
+
             MeshRenderer cloneRenderer = clone.GetComponent<MeshRenderer>();
             
 
@@ -45,7 +61,7 @@ public class Shadows : MonoBehaviour
             cloneRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
 
             float yScale = (renderer.bounds.size.y - playerHeight) / renderer.bounds.size.y;
-            clone.transform.localScale = new Vector3(clone.transform.localScale.x, yScale, clone.transform.localScale.z);
+            emptyParent.transform.localScale = new Vector3(emptyParent.transform.localScale.x, yScale, emptyParent.transform.localScale.z);
             
             //original.transform.rotation = oRotation;
 
