d01427b79e36ead4ae1c6595122cbd95837fd2d3
hobbes6378 <68025392+hobbes6378@users.noreply.github.com>
2024-02-21 13:50:52 -0800

    Inventroy object move

--- Assets/Scriptable Objects/Inventory/Scripts/DisplayInventory.cs
+++ Assets/Scriptable Objects/Inventory/Scripts/DisplayInventory.cs
@@ -14,13 +14,13 @@ public class DisplayInventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateDisplay();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateDisplay();
     }
 
     public void CreateDisplay()
@@ -28,11 +28,31 @@ public class DisplayInventory : MonoBehaviour
         for (int i = 0; i < inventory.Containter.Count; i++)
         {
             var obj = Instantiate(inventory.Containter[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
-            obj.GetComponent<Transform>().localPosition = new Vector3(0, 0, 0);
+            obj.GetComponent<Transform>().localPosition = GetPosition(i);
+            // create the count of hte object
+            itemsDisplayed.Add(inventory.Containter[i], obj);
         }
     }
     public Vector3 GetPosition(int i)
     {
         return new Vector3(X_SPACE_BETWEEN_ITEM*(i % NUMBER_OF_COLUMN), (-Y_SPACE_BETWEEN_ITEM * (i/NUMBER_OF_COLUMN)), 0f);
+    }
+
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Containter.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(inventory.Containter[i]))
+            {
+                //change the count of the object
+            }
+            else
+            {
+                var obj = Instantiate(inventory.Containter[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<Transform>().localPosition = GetPosition(i);
+                // create the count of hte object
+                itemsDisplayed.Add(inventory.Containter[i], obj);
+            }
+        }
     }
 }
