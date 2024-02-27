651de4357ad78ca22d5fb7c164aef0a5de571413
KryptKrawler <73673768+KryptKrawler@users.noreply.github.com>
2024-02-27 12:39:31 -0800

    Removed Some Extremely Bugged Code

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    float OnCollisionEnter(Collision collision)
+    /*float OnCollisionEnter(Collision collision)
 	{
 		Vector3 normal = collision.contacts[0].normal;
 
 		float collisionAngleTest1 = Vector3.Angle(Vector3.zero, -normal);
 
 		return collisionAngleTest1;
-    }
+    }*/
 }
