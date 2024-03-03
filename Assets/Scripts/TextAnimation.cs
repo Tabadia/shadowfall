91af16f5aa717eead867578fa1dc86192a145e39
KryptKrawler <73673768+KryptKrawler@users.noreply.github.com>
2024-03-02 18:07:47 -0800

    Added Regex to allow custom color tags for console messages

--- Assets/Scripts/TextAnimation.cs
+++ Assets/Scripts/TextAnimation.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public class TextAnimation : MonoBehaviour
 {
@@ -17,25 +18,56 @@ public class TextAnimation : MonoBehaviour
 
     void Start()
     {
-        AddMessage("Testing Database XA34IL Connection............\r\nERROR\r\nTesting Main Connection....\r\nERROR\r\nTesting Cryopod ID 6749 Activation Code.....\r\nERROR\r\nManual Assistance Required\r\nCalling Operations Assistant......\r\nERROR\r\nActivating Function Code \"LAST STRAW\"....\r\nSUCCESS\r\nG o o d   M o r n i n g\r\n\r\n");
+        AddMessage("Testing Database XA34IL Connection:\n" +
+            "<color=red>ERROR</color>\n" +
+            "Testing Main Connection:\n" +
+            "<color=red>ERROR</color>\n" +
+            "Testing Cryopod ID 6749 Activation Code:\n" +
+            "<color=red>ERROR</color>\n" +
+            "Manual Assistance Required\n" +
+            "Calling Operations Assistant:\n" +
+            "<color=red>ERROR</color>\n" +
+            "Activating Emergency Function Code AWAKE:\n" +
+            "<color=green>SUCCESS</color>\n" +
+            "G o o d   M o r n i n g\n");
         StartCoroutine(UpdateConsole());
-        //StartCoroutine(BlinkCursor());
-       
     }
 
     IEnumerator UpdateConsole()
     {
         foreach (string message in messageQueue)
         {
-            int messageLength = message.Length;
+            string[] parts = Regex.Split(message, @"(<.*?>)"); // Split the message using color tags as delimiters
+            string typedMessage = "";
 
-            for (int i = 0; i <= messageLength; i++)
+            for (int i = 0; i < parts.Length; i++)
             {
-                consoleText.text = message.Substring(0, i) + (cursorVisible ? "_" : "");
-                yield return new WaitForSeconds(letterDelay);
+                if (parts[i].StartsWith("<") && parts[i].EndsWith(">"))
+                {
+                    // If the part is a color tag, just append it to the typed message
+                    typedMessage += parts[i];
+                }
+                else
+                {
+                    // If the part is text, type it letter by letter
+                    for (int j = 0; j < parts[i].Length; j++)
+                    {
+                        typedMessage += parts[i][j];
+                        consoleText.text = typedMessage + (cursorVisible ? "_" : "");
+                        yield return new WaitForSeconds(letterDelay);
+                    }
+                }
+
+                // Check if the typed message ends with a newline character
+                if (typedMessage.EndsWith("\n"))
+                {
+                    yield return new WaitForSeconds(lineDelay);
+                }
             }
 
-            yield return new WaitForSeconds(lineDelay); // Delay between lines
+            // Display the complete typed message
+            consoleText.text = typedMessage;
+            yield return new WaitForSeconds(lineDelay); // Wait for lineDelay after displaying each message
         }
     }
 
