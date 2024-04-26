286fb5f3e705356379dcb3f973923c961ba8e4cd
KryptKrawler <73673768+KryptKrawler@users.noreply.github.com>
2024-04-26 12:46:51 -0700

    More Fixes to Pause Menu System

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -31,8 +31,20 @@ public class PauseMenu : MonoBehaviour
             
             
             }
-            
-        
+
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+
+            }
+
+
         }
         
     }
