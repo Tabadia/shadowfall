c13c69dbeb1b5ddef31065bc160afa631d380bbc
Thalen <thalenabadia@gmail.com>
2024-05-06 15:07:32 -0700

    Revert "more invo"
    This reverts commit 6a7960342c40106cb0d876051b96436fb97672ff.

--- Assets/Scriptable Objects/Items/Scripts/ItemObject.cs
+++ Assets/Scriptable Objects/Items/Scripts/ItemObject.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public enum ItemType
@@ -50,13 +48,7 @@ public class Item
     {
         Name = item.name;
         Id = item.data.Id;
-        if (AssetDatabase.FindAssets(Name).Length <= 0)
-        {
-            return;
-        }
-        string GUID = AssetDatabase.FindAssets(Name)[0];
-        string PATH = AssetDatabase.GUIDToAssetPath(GUID);
-        obj = AssetDatabase.LoadAssetAtPath<GameObject>(PATH);
+        obj = GameObject.Find(Name);
         buffs = new ItemBuff[item.data.buffs.Length];
         for(int i = 0; i < buffs.Length; i++)
         {
