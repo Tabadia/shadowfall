7a761a913a4c0932167d1be7a4c91610e37ff222
Thalen <thalenabadia@gmail.com>
2024-05-18 01:25:30 -0700

    terminal commands

--- Assets/Scripts/Interpreter.cs
+++ Assets/Scripts/Interpreter.cs
@@ -22,8 +22,9 @@ public class Interpreter : MonoBehaviour
         if (args[0] == "help")
         {
 
-            response.Add("If you want to use the terminal, type \"boop\" ");
-            response.Add("We're testing the potential outputs");
+            response.Add("Type \"logs\" to see the logs you have discovered");
+            response.Add("Type \"credits\" to see the credits");
+            response.Add("Type \"company-secrets\" to see the company secrets");
 
         }
         else if (args[0] == "ping")
@@ -44,6 +45,19 @@ public class Interpreter : MonoBehaviour
                     response.Add(log);
                 }
             }
+        }
+        else if (args[0] == "credits"){
+            response.Add("Tanush Sistla - Team Manager & Programmer");
+            response.Add("Thalen Abadia - Lead Programmer");
+            response.Add("Angela Zhang - Programmer & Writer");
+            response.Add("Phoenix Jones - Audio Producer");
+            response.Add("Wesley Dean - Writer");
+            response.Add("Declan Chamberlain - 2D Artist");
+            response.Add("Teo Fine - 3D Modeler");
+        }
+        else if (args[0] == "company-secrets"){
+            response.Add("Your actions against the company have been brought to the attention of Solis Corp's security team.");
+            response.Add("You have been marked as a threat to the company.");
         }
         else if (args[0] == "shutdown /s")
         {
